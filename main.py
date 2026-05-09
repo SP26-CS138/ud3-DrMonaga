5373d80d6dfc37bbd7b1f278bb9fdde2bc6376a9
DrMonaga <semonagas@icloud.com>
2026-05-09 00:12:46 -0700

    sdsa

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ DATE: 4/24
 """
 Golf scorecard tracker which takes input for scoes and then formats it nicely so you can see scores at end. I limited it to 3 holes just for demonstration
 
-Confused of where to explain why I used list over dictionary but I used a list over a dictionary because with a scorecard the values and the amount of values is constantly changing so a list would be ideal so I can change the values much easier than using a dicitonary. 
-
 Leave one blank line.  The rest of this docstring should contain an
 overall description of the program.
 """
@@ -25,21 +23,21 @@ INPUT_FILE = "input_file.txt"
 # FUNCTIONS:
 ##########################################
 # <replace this line with function definitions, each needs a description>
-def write_scorecard(x):
+def write_scorecard(scores):
     file = open(INPUT_FILE, "w")
-    for y in x:
-        file.write(str(y) + " ")
+    for score in scores:
+        file.write(str(score) + " ")
     file.close()
 
 def show_scorecard():
     print("===== SCORECARD =====")
     hole = 1
     file = open(INPUT_FILE, "r")
-    s = file.read()
-    z = s.split()
-    for i in z:
-        y = int(i)
-        print(f"Hole {hole}: {y} strokes")
+    scores = file.read()
+    split_scores = scores.split()
+    for hole_number in split_scores:
+        stroke_number = int(hole)
+        print(f"Hole {hole}: {hole_number} strokes")
         hole = hole + 1
     file.close()
 
